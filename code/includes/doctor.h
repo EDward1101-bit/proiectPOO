3235bef04941e4edee90215efc5f457d5114ceb7
Smărăndoiu Eduard-Ștefan <e.smarandoiu@yahoo.com>
2025-03-30 21:35:00 +0300

    adaugare zile doctori+noi functionalitati

--- code/includes/doctor.h
+++ code/includes/doctor.h
@@ -3,22 +3,35 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Doctor {
 private:
     std::string name;
     std::string specialty;
+    std::vector<std::string> availableDays;  // Days when the doctor is available
 
 public:
     // Constructor
-    explicit Doctor(std::string name, std::string specialty);
+    Doctor(std::string name, std::string specialty, std::vector<std::string> availableDays);
+
+    // Copy Constructor
+    Doctor(const Doctor& other);
+
+    // Copy Assignment Operator
+    Doctor& operator=(const Doctor& other);
+
+    // Destructor
+    ~Doctor();
 
     // Public member functions
     void changeSpecialty(const std::string& newSpecialty);
+    void addAvailableDay(const std::string& day);
     void printInfo() const;
+    bool isAvailableOn(const std::string& day) const;
 
     // Operator<< Overload
     friend std::ostream& operator<<(std::ostream& os, const Doctor& d);
 };
 
-#endif//DOCTOR_H
+#endif // DOCTOR_H
