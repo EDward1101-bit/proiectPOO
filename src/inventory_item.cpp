d163c1152586c43606dac936e7593a3d031eca67
Smărăndoiu Eduard-Ștefan <e.smarandoiu@yahoo.com>
2025-06-08 14:37:02 +0300

    implementare baza inventory

---
+++ src/inventory_item.cpp
@@ -0,0 +1,21 @@
+#include "../includes/inventory_item.h"
+
+InventoryItem::InventoryItem() : id(++current_id), name("Unnamed"), price(0.0) {}
+
+InventoryItem::InventoryItem(const std::string& name, double price)
+    : id(++current_id), name(name), price(price) {}
+
+InventoryItem::InventoryItem(const InventoryItem& other)
+    : id(++current_id), name(other.name), price(other.price) {}
+
+int InventoryItem::getId() const {
+    return id;
+}
+
+double InventoryItem::priceValue() const {
+    return price;
+}
+
+bool InventoryItem::hasName(const std::string& query) const {
+    return name == query;
+}
